59ed11da207e20a59e864c4d1fa29445c44b7b49
James Munnelly <james.munnelly@jetstack.io>
2017-03-21 14:46:43 +0000

    Switch roles and plugins to be encoded as JSON

--- sidecar/cmd/start.go
+++ sidecar/cmd/start.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
 
@@ -25,8 +27,8 @@ var (
 	esSidecarPassword string
 	clusterURL        string
 
-	pluginsFlag []string
-	rolesFlag   []string
+	pluginsFlag string
+	rolesFlag   string
 
 	startCmd = &cobra.Command{
 		Use:   "start",
@@ -130,38 +132,14 @@ func parsePluginsFlag() error {
 	if len(pluginsFlag) == 0 {
 		return nil
 	}
-
-	if len(pluginsFlag) == 1 && pluginsFlag[0] == "" {
-		return nil
-	}
-
-	pl := make([]v1.ElasticsearchClusterPlugin, len(pluginsFlag))
-	for i, plugin := range pluginsFlag {
-		pl[i] = v1.ElasticsearchClusterPlugin{
-			Name: plugin,
-		}
-	}
-
-	plugins = pl
-	return nil
+	return json.Unmarshal([]byte(pluginsFlag), &plugins)
 }
 
 func parseRolesFlag() error {
 	if len(rolesFlag) == 0 {
-
-	}
-
-	if len(rolesFlag) == 1 && rolesFlag[0] == "" {
 		return nil
 	}
-
-	r := make([]util.Role, len(rolesFlag))
-	for i, role := range rolesFlag {
-		r[i] = util.Role(role)
-	}
-
-	roles = r
-	return nil
+	return json.Unmarshal([]byte(rolesFlag), &roles)
 }
 
 func init() {
@@ -169,8 +147,8 @@ func init() {
 	startCmd.PersistentFlags().StringVar(&podName, "podName", "", "The name of this pod")
 	startCmd.PersistentFlags().StringVar(&namespace, "namespace", "", "The namespace this node is running in")
 	startCmd.PersistentFlags().StringVar(&apiServerHost, "apiServerHost", "", "Kubernetes apiserver host address (overrides autodetection)")
-	startCmd.PersistentFlags().StringSliceVarP(&pluginsFlag, "plugins", "p", []string{}, "List of Elasticsearch plugins to install")
-	startCmd.PersistentFlags().StringSliceVarP(&rolesFlag, "roles", "r", []string{}, "The role of this Elasticsearch node")
+	startCmd.PersistentFlags().StringVarP(&pluginsFlag, "plugins", "p", "", "List of Elasticsearch plugins to install")
+	startCmd.PersistentFlags().StringVarP(&rolesFlag, "roles", "r", `["client"]`, "The role of this Elasticsearch node")
 	startCmd.PersistentFlags().StringVar(&controllerName, "controllerName", "", "Name of the controller managing this node")
 	startCmd.PersistentFlags().StringVar(&controllerKind, "controllerKind", "", "Kind of the controller managing this node")
 	startCmd.PersistentFlags().StringVar(&clusterURL, "clusterURL", "", "URL for communicating with Elasticsearch client nodes")
